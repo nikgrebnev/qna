b2a58f95bae3d8d380907185763cf34696b75952
Nik Grebnev <nikolaygrebnev@gmail.com>
2020-01-12 13:08:21 +0300

    lesson6 question index

---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'Show all questions', %q{
+  In order to show answers
+  As an user
+} do
+  given(:user) { create(:user) }
+  given!(:questions) { create_list(:question, 10) }
+
+  scenario 'Index questions' do
+    visit questions_path
+
+    questions.each do |question|
+      expect(page).to have_content question.title
+    end
+  end
+
+end
