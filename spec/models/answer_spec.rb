663b54863c98f454e43619993ba03cf8486a39b1
Nik Grebnev <nikolaygrebnev@gmail.com>
2020-01-23 21:18:22 +0300

    lesson 7a, 1-18

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -17,9 +17,26 @@ RSpec.describe Answer, type: :model do
       question.answers.reload
       expect(question.answers).to_not eq answers
     end
+  end
 
-    describe 'test make_best' do
+  describe 'test make_best' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: user) }
+    let(:answers) { create_list(:answer, 2, question: question, user: user) }
 
+    it 'set best' do
+      best_answer = answers.first
+      best_answer.make_best!
+      expect(best_answer.best?).to be_truthy
+      expect(answers.last.best?).to be_falsey
+    end
+
+    it 'set another' do
+      answers.first.make_best!
+      answers.last.make_best!
+      answers.first.reload
+      expect(answers.first.best?).to be_falsey
+      expect(answers.last.best?).to be_truthy
     end
   end
 end
