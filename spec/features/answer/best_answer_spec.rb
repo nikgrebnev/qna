c4476e1968aa50a8417c0180fd7cb2e40fb17d89
Nik Grebnev <nikolaygrebnev@gmail.com>
2020-02-07 21:03:39 +0300

    lesson 9, 1-5 fix

--- spec/features/answer/best_answer_spec.rb
+++ spec/features/answer/best_answer_spec.rb
@@ -5,18 +5,34 @@ feature 'best answer', %q{
 
   given!(:user) { create(:user) }
   given!(:author) { create(:user) }
-  given!(:question) { create(:question, user: author) }
+  given!(:question) { create(:question, :with_reward,  user: author) }
   given!(:answers) { create_list(:answer, 10, question: question, user: user) }
 
-  scenario 'author can choose best answer', js: true do
-    log_in(author)
-    visit question_path(question)
+  describe 'best answers main options' do
+    background do
+      log_in(author)
+      visit question_path(question)
 
-    expect(page).to_not have_css(".best-answer")
-    expect(page).to have_link 'Make best answer'
-    click_on 'Make best answer' , :match => :first
-    expect(page).to have_content 'Best Answer'
-    expect(page).to have_css(".best-answer")
+      expect(page).to_not have_css(".best-answer")
+      expect(page).to have_link 'Make best answer'
+      click_on 'Make best answer' , :match => :first
+    end
+
+    scenario 'author can choose best answer', js: true do
+      expect(page).to have_content 'Best Answer'
+      expect(page).to have_css(".best-answer")
+    end
+
+    scenario 'author of answer can see reward in rewards list', js: true do
+      log_out
+      log_in(user)
+
+      visit rewards_path
+
+      expect(page).to have_content question.title
+      expect(page).to have_content question.reward.name
+      expect(page).to have_css("img[src*='test1.jpg']")
+    end
   end
 
   scenario 'not author can not choose best answer', js: true do
