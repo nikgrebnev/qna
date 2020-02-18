8aa26ae3e96466827909f3a438ec4cd3ac434c2e
Nik Grebnev <nikolaygrebnev@gmail.com>
2020-02-18 17:28:48 +0300

    lesson 10, 2-3

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -11,8 +11,6 @@ class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true, touch: true
 
   validates :value, inclusion: { in: [-1, 1] }, presence: true
-  validates :votable_id, uniqueness: { scope: %i[votable_type user_id], message: 'can vote once!' }
+  validates_uniqueness_of :votable_id, scope: [:votable_type, :user_id], message: 'can vote once!'
   validates_with VoteUserValidator
 end
-
-
