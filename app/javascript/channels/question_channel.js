db0ee85fbc7e53162bf0ad5d993eb8dcca1c76de
Nik Grebnev <nikolaygrebnev@gmail.com>
2020-03-10 19:25:36 +0300

    lesson 11, 1-2

--- app/javascript/channels/question_channel.js
+++ app/javascript/channels/question_channel.js
@@ -5,39 +5,49 @@ $(document).on('turbolinks:load', function () {
     consumer.subscriptions.remove(this.subscription);
   }
 
-  var subscription = consumer.subscriptions.create( {channel: 'QuestionChannel', room: gon.question_id }, {
-    received(data) {
-      if (data['event'] === 'new_answer' && data['question_author_id'] != gon.user_id) {
-        var answers = $('.answers');
-        if (answers) {
-          var answer = renderAnswer(data['answer'], data['question_author_id']);
-          var div = document.createElement('div');
-          div.setAttribute('data-id', data['answer']['id']);
-          div.setAttribute('id', "answer" + data['answer']['id']);
-          div.classList.add('answer');
-          div.innerHTML = answer;
-          answers.append(div);
+  var route = window.location.href.split('/').slice(-2);
+  var question_id = 0;
+  if ((route[0] = 'questions') && (route[1].match(/\d/)))
+  {
+    question_id = route[1];
+  }
+
+  if (question_id > 0) {
+    var subscription = consumer.subscriptions.create( {channel: 'QuestionChannel', room: question_id }, {
+      
+      received(data) {
+        if (data['event'] === 'new_answer' && data['question_author_id'] != gon.user_id) {
+          var answers = $('.answers');
+          if (answers) {
+            var answer = renderAnswer(data['answer'], data['question_author_id']);
+            var div = document.createElement('div');
+            div.setAttribute('data-id', data['answer']['id']);
+            div.setAttribute('id', "answer" + data['answer']['id']);
+            div.classList.add('answer');
+            div.innerHTML = answer;
+            answers.append(div);
+          }
         }
-      }
-      if (data['event'] === 'new_comment'){
-        var resource_type = data['resource_type'];
+        if (data['event'] === 'new_comment'){
+          var resource_type = data['resource_type'];
 
-        if (resource_type === 'question') {
-          var resource = $('.question-comments');
+          if (resource_type === 'question') {
+            var resource = $('.question-comments');
+          }
+          else if (resource_type === 'answer') {
+            var resource = $('.answers div[data-id=' + data['resource_id'] + '] .answer-comments');
+          };
+
+          if ((resource) && (data['comment']['user_id'] != gon.user_id)) {
+            var comment = renderComment(data['comment']);
+            resource.append(comment);
+          };
         }
-        else if (resource_type === 'answer') {
-          var resource = $('.answers div[data-id=' + data['resource_id'] + '] .answer-comments');
-        };
+      },
+    });
+    this.subscription = subscription;
+  }
 
-        if ((resource) && (data['comment']['user_id'] != gon.user_id)) {
-          var comment = renderComment(data['comment']);
-          resource.append(comment);
-        };
-      }
-    },
-  });
-
-  this.subscription = subscription;
 })
 
 function renderAnswer(answer, question_author_id) {
