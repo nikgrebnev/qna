444f645da94f2c7b0a41f5bd2121b1f6bc40b6b8
Nik Grebnev <nikolaygrebnev@gmail.com>
2020-05-20 22:40:18 +0300

    part 3

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Ability do
+  subject(:ability) { Ability.new(user) }
 
+  describe 'for quest' do
+    let(:user) { nil }
+
+    it {should be_able_to :read, Question}
+    it {should be_able_to :read, Answer}
+    it {should be_able_to :read, Comment}
+
+    it {should_not be_able_to :manage, :all}
+  end
+
+  describe 'for admin' do
+    let(:user) { create(:admin) }
+
+    it { should be_able_to :manage, :all }
+  end
 
 end
